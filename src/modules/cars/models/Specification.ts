40a8f9dc2b5204b021af16a93aa37b2a979f4d93
mtheusbrito <matheusbritodasilva@gmail.com>
2021-09-09 09:46:55 -0300

    Criando service de especificação e separando em módulos

---
+++ src/modules/cars/models/Specification.ts
@@ -0,0 +1,15 @@
+import { v4 } from "uuid";
+
+class Specification {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = v4();
+    }
+  }
+}
+export { Specification };
