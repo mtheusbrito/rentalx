f4dd59149ee585d176379fbd5a7aef39e9111ed1
mtheusbrito <matheusbritodasilva@gmail.com>
2021-09-12 22:20:39 -0300

    Criando repositorio de especificacoes.

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,26 @@
+import { Specification } from "../models/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+  findByName(name: string): Specification {
+    const specification = this.specifications.find((s) => s.name === name);
+    return specification;
+  }
+  create({ description, name }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, { name, description, created_at: new Date() });
+
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationsRepository };
